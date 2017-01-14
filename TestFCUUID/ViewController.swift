d679cf31e82bc4848ac3bea0297fc6e02a1eecca
Sayalic <sayalic@qlcoder.com>
2017-01-14 16:21:17 +0800

    Initial Commit

---
+++ TestFCUUID/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestFCUUID
+//
+//  Created by 正翔  沈 on 14/01/2017.
+//  Copyright © 2017 Sayalic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
